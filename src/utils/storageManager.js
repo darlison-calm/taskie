f1a5324066c18d197268a843880e41d5ba93a427
darlisonDev <bernardo.darlison@gmail.com>
2024-09-18 21:40:11 -0300

    Trying to fix bug
    Tring to fix bug of editing two task at the same time

--- src/utils/storageManager.js
+++ src/utils/storageManager.js
@@ -12,7 +12,6 @@ export function loadTaskState() {
   const tasksLoaded = savedTasks.map((json) => {
     const task = new Task(json._title, json._dueDate, json._priority, json._description, json._projectId);
     task.complete = json._complete
-    task.id = json._id
     return task
   })
   taskManager.setTasks(tasksLoaded);
